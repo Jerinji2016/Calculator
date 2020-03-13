673a5ffa3ee244c067a9e75066b50eb384e3c1d1
Jerinji2016 <jerinji2016@gmail.com>
2020-03-13 14:22:37 +0530

    Icon Added Calculation and Object Updation Answer display

---
+++ app/src/main/java/com/learn/calculator/Calculation.java
@@ -0,0 +1,52 @@
+package com.learn.calculator;
+
+public class Calculation {
+    private String operand1;
+    private String operand2;
+    private boolean opFlag;
+    private char currentOp;
+    private double answer;
+
+    Calculation() {
+    }
+
+    public String getOperand1() {
+        return operand1;
+    }
+
+    void setOperand1(String operand1) {
+        this.operand1 = operand1;
+    }
+
+    public String getOperand2() {
+        return operand2;
+    }
+
+    void setOperand2(String operand2) {
+        this.operand2 = operand2;
+    }
+
+    public boolean isOpFlag() {
+        return opFlag;
+    }
+
+    public void setOpFlag(boolean opFlag) {
+        this.opFlag = opFlag;
+    }
+
+    public char getCurrentOp() {
+        return currentOp;
+    }
+
+    void setCurrentOp(char currentOp) {
+        this.currentOp = currentOp;
+    }
+
+    public double getAnswer() {
+        return answer;
+    }
+
+    void setAnswer(double answer) {
+        this.answer = answer;
+    }
+}
--- app/src/main/java/com/learn/calculator/CalculatorUpdate.java
+++ app/src/main/java/com/learn/calculator/CalculatorUpdate.java
@@ -2,32 +2,69 @@ package com.learn.calculator;
 
 import android.content.Context;
 import android.widget.TextView;
-import android.widget.Toast;
 
 class CalculatorUpdate {
-    static void Updater(TextView t, char s, Context context) {
+    static void Updater(TextView t, char s, Context context, Calculation calculation) {
         String temp = (String) t.getText();
-        if(temp.length() == 0) {
+        if (checkOp(t, s, context, calculation)) {          //  Check for consecutive operator repeat
+            //  Continue to append value
             temp += s;
             t.setText(temp);
-        }
-        else {
-            if(checkOp(t,s,context)) {
-                temp += s;
-                t.setText(temp);
+
+            if (Character.getNumericValue(s) >= 0 && Character.getNumericValue(s) <= 9 || s == '.') {
+                calculation.setOperand2(calculation.getOperand2()+s);
             }
-            else
-                Toast.makeText(context, "Mairu repeat", Toast.LENGTH_SHORT).show();
+            else if (Character.getNumericValue(s) < 0) {
+                double n1 = Double.parseDouble(calculation.getOperand1());
+                double n2= Double.parseDouble(calculation.getOperand2());
+                switch (calculation.getCurrentOp()) {
+                    case '+' :
+                        n1 = n1 + n2;
+                        break;
+                    case '-' :
+                        n1 = n1 - n2;
+                        break;
+                    case '*' :
+                        n1 = n1 * n2;
+                        break;
+                    case '÷' :
+                        n1 = n1 / n2;
+                        break;
+                    case '%' :
+                        n1 = n1 % n2;
+                        break;
+                }
+                calculation.setOperand1(Double.toString(n1));
+                calculation.setOperand2("");
+                calculation.setCurrentOp(s);
+                calculation.setOpFlag(true);
+            }
         }
     }
 
-    private static boolean checkOp(TextView t, char c, Context context) {
+    private static boolean checkOp(TextView t, char c, Context context, Calculation calculation) {
         String last_char = (String) t.getText();
+
+        if(last_char.length() == 0) {
+            if(Character.getNumericValue(c) < 0)
+                return false;
+            else
+                return true;
+        }
         char x = last_char.charAt(last_char.length() - 1);
         String ops = "+-*÷%.";
-        if (ops.indexOf(c) >= 0 && ops.indexOf(x) >= 0)
+
+        if (ops.indexOf(c) >= 0 && ops.indexOf(x) >= 0) {
             return false;
-        else
+        }
+        else {
+            if(c == '.' && calculation.isOpFlag()) {
+                calculation.setOpFlag(false);
+                return true;
+            }
+            else if(!calculation.isOpFlag() && c == '.')
+                return false;
             return true;
+        }
     }
 }
